bae097e298ac8a98a2dfea51b5e674f56bcda5eb
Jakub S <jskupien@leon.pl>
2020-09-12 18:20:19 +0200

    init

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        sa
         </p>
         <a
           className="App-link"
