41c77e6992cb5884490af1ddef664153b0e07b99
Jakub S <jskupien@leon.pl>
2020-09-13 15:53:45 +0200

    poprawione feat

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        testowane from feature
+        testowane porawione feature
         </p>
         <a
           className="App-link"
